6608de2676ba0c8d10530de034378396303ba234
Nicolas Reymond <n.reymond.12@gmail.com>
2020-03-20 17:28:59 +0100

    Explore Python on your own

--- get-quote.py
+++ get-quote.py
@@ -1,16 +1,24 @@
 import random
+import sys
 
 def primary():
-  # print("Keep it logically awesome.")
 
-  f = open("quotes.txt")
-  quotes = f.readlines()
-  f.close()
+    if len(sys.argv)>1:
+        f = open("quotes.txt", "a")
+        f.write(sys.argv[1])
+        f.close()
+    else:
+        f = open("quotes.txt")
+        quotes = f.readlines()
+        f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
+        last = 15
 
-  print(quotes[rnd])
+        rnd2 = random.randint(0, last)
+
+        for x in range(rnd2):
+            print(quotes[random.randint(0, last)]),
+
 
 if __name__== "__main__":
-  primary()
+    primary()
